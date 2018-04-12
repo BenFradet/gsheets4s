fa80d915b8ede4d695a4162f37caf8151ce01291
Ben Fradet <benjamin.fradet@gmail.com>
2018-04-12 19:08:04 +0100

    Migrated to cats.Show

--- src/main/scala/gsheets4s/model.scala
+++ src/main/scala/gsheets4s/model.scala
@@ -3,8 +3,10 @@ package gsheets4s
 import atto._
 import atto.Atto._
 import atto.syntax.refined._
-import cats.implicits._
+import cats.Show
+import cats.syntax.apply._
 import cats.syntax.either._
+import cats.syntax.show._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.char._
 import eu.timepit.refined.collection._
@@ -12,15 +14,18 @@ import eu.timepit.refined.numeric._
 import io.circe.{Decoder, DecodingFailure, Encoder, HCursor}
 
 object model {
-  type ValidRow = Positive
-  type Row = Int Refined ValidRow
   type ValidCol = Forall[UpperCase]
   type Col = String Refined ValidCol
+  type ValidRow = Positive
+  type Row = Int Refined ValidRow
 
-  sealed trait Position {
-    def stringRepresentation: String
-  }
+  sealed trait Position
   object Position {
+    implicit val showPosition: Show[Position] = Show.show {
+      case ColPosition(c) => c.toString
+      case RowPosition(r) => r.toString
+      case ColRowPosition(c, r) => s"$c${r.toString}"
+    }
     val parser: Parser[Position] = {
       val colP = stringOf1(upper).refined[ValidCol]
       val rowP = int.refined[ValidRow]
@@ -29,41 +34,31 @@ object model {
         (colP, rowP).mapN(ColRowPosition(_, _): Position)
     }
   }
-  final case class ColPosition(col: Col) extends Position {
-    override def stringRepresentation: String = col.toString
-  }
-  final case class RowPosition(row: Row) extends Position {
-    override def stringRepresentation: String = row.toString
-  }
-  final case class ColRowPosition(col: Col, row: Row) extends Position {
-    override def stringRepresentation: String = s"$col${row.toString}"
-  }
+  final case class ColPosition(col: Col) extends Position
+  final case class RowPosition(row: Row) extends Position
+  final case class ColRowPosition(col: Col, row: Row) extends Position
 
-  final case class Range(start: Position, end: Position) {
-    def stringRepresentation: String = s"${start.stringRepresentation}:${end.stringRepresentation}"
-  }
+  final case class Range(start: Position, end: Position)
   object Range {
+    implicit val showRange: Show[Range] = Show.show(r => s"${r.start.show}:${r.end.show}")
     val parser: Parser[Range] = (Position.parser <~ Atto.char(':'), Position.parser).mapN(Range.apply)
   }
 
-  sealed trait A1Notation {
-    def stringRepresentation: String
-  }
+  sealed trait A1Notation
   object A1Notation {
+    implicit val showA1Notation: Show[A1Notation] = Show.show {
+      case SheetNameNotation(s) => s
+      case RangeNotation(r) => r.show
+      case SheetNameRangeNotation(s, r) => s"$s!${r.show}"
+    }
     val parser: Parser[A1Notation] =
       (takeWhile(_ != '!') <~ Atto.char('!'), Range.parser).mapN(SheetNameRangeNotation.apply) |
         Range.parser.map(RangeNotation(_): A1Notation) |
         stringOf1(elem(_ => true)).map(SheetNameNotation(_): A1Notation)
   }
-  final case class SheetNameNotation(sheetName: String) extends A1Notation {
-    override def stringRepresentation: String = sheetName
-  }
-  final case class RangeNotation(range: Range) extends A1Notation {
-    override def stringRepresentation: String = range.stringRepresentation
-  }
-  final case class SheetNameRangeNotation(sheetName: String, range: Range) extends A1Notation {
-    override def stringRepresentation: String = s"$sheetName!${range.stringRepresentation}"
-  }
+  final case class SheetNameNotation(sheetName: String) extends A1Notation
+  final case class RangeNotation(range: Range) extends A1Notation
+  final case class SheetNameRangeNotation(sheetName: String, range: Range) extends A1Notation
   implicit val a1NotationDecoder: Decoder[A1Notation] = Decoder.decodeString.flatMap { s =>
     new Decoder[A1Notation] {
       final def apply(c: HCursor): Decoder.Result[A1Notation] =
@@ -72,7 +67,7 @@ object model {
     }
   }
   implicit val a1NotationEncoder: Encoder[A1Notation] =
-    Encoder.encodeString.contramap(_.stringRepresentation)
+    Encoder.encodeString.contramap(_.show)
 
   sealed abstract class Dimension(val value: String)
   case object Rows extends Dimension("ROWS")
