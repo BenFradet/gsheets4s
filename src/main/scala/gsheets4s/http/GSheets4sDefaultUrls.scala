80fc8cc2d67728ea9417854b15bd348ea280d30d
Ben Fradet <benjamin.fradet@gmail.com>
2018-05-13 20:14:49 +0100

    Encapsulate defaults urls in a case class

---
+++ src/main/scala/gsheets4s/http/GSheets4sDefaultUrls.scala
@@ -0,0 +1,16 @@
+package gsheets4s
+
+import cats.syntax.option._
+import hammock.Uri
+
+case class GSheets4sDefaultUrls(
+  baseUrl: Uri,
+  refreshTokenUrl: Uri
+)
+
+object GSheets4sDefaultUrls {
+  implicit val defaultUrls: GSheets4sDefaultUrls = GSheets4sDefaultUrls(
+    Uri("https".some, none, "sheets.googleapis.com/v4/spreadsheets"),
+    Uri("https".some, none, "www.googleapis.com/oauth2/v4/token")
+  )
+}
