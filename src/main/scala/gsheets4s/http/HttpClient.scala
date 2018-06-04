58660bc89fdbd37e7484105903a018d520b6040a
Ben Fradet <benjamin.fradet@gmail.com>
2018-06-04 23:17:21 +0100

    HttpClient handling token refreshing and retrying

--- src/main/scala/gsheets4s/http/HttpClient.scala
+++ src/main/scala/gsheets4s/http/HttpClient.scala
@@ -34,34 +34,46 @@ class HammockRequester[F[_]: Sync](implicit nat: HammockF ~> F) extends HttpRequ
     }
 }
 
-class HttpClient[F[_]: Monad](creds: Ref[F, Credentials])(
-    implicit urls: GSheets4sDefaultUrls, requester: HttpRequester[F]) {
+class HttpClient[F[_]](creds: Ref[F, Credentials])(
+    implicit urls: GSheets4sDefaultUrls, requester: HttpRequester[F], M: Monad[F]) {
   def get[O](
     path: String,
     params: List[(String, String)] = List.empty)(
-    implicit d: Decoder[O]): F[Either[GsheetsError, O]] = for {
-      c <- creds.get
-      res <- requester
-        .request[Either[GsheetsError, O]](urlBuilder(c.accessToken, path, params), Method.GET)
-    } yield res
+    implicit d: Decoder[O]): F[Either[GsheetsError, O]] =
+      req(token => requester
+        .request[Either[GsheetsError, O]](urlBuilder(token, path, params), Method.GET))
 
   def put[I, O](
     path: String,
     body: I,
     params: List[(String, String)] = List.empty)(
-    implicit e: Encoder[I], d: Decoder[O]): F[Either[GsheetsError, O]] = for {
-      c <- creds.get
-      res <- requester.requestWithBody[I, Either[GsheetsError, O]](
-        urlBuilder(c.accessToken, path, params), body, Method.PUT)
-    } yield res
+    implicit e: Encoder[I], d: Decoder[O]): F[Either[GsheetsError, O]] =
+      req(token => requester.requestWithBody[I, Either[GsheetsError, O]](
+        urlBuilder(token, path, params), body, Method.PUT))
 
-  def refreshToken(creds: Credentials)(implicit d: Decoder[String]): F[String] = {
+  private def req[O](req: String => F[Either[GsheetsError, O]]): F[Either[GsheetsError, O]] = for {
+    c <- creds.get
+    first <- req(c.accessToken)
+    retried <- first match {
+      case Left(GsheetsError(401, _, _)) => reqWithNewToken(req, c)
+      case o => M.pure(o)
+    }
+  } yield retried
+
+  private def reqWithNewToken[O](
+    req: String => F[Either[GsheetsError, O]], c: Credentials): F[Either[GsheetsError, O]] = for {
+      newToken <- refreshToken(c)(Decoder.decodeString.prepare(_.downField("access_token")))
+      _ <- creds.setAsync(c.copy(accessToken = newToken))
+      r <- req(newToken)
+    } yield r
+
+  private def refreshToken(c: Credentials)(implicit d: Decoder[String]): F[String] = {
     val url = urls.refreshTokenUrl ?
       NonEmptyList(
-        ("refresh_token" -> creds.refreshToken),
+        ("refresh_token" -> c.refreshToken),
         List(
-          ("client_id" -> creds.clientId),
-          ("client_secret" -> creds.clientSecret),
+          ("client_id" -> c.clientId),
+          ("client_secret" -> c.clientSecret),
           ("grant_type" -> "refresh_token")
         )
       )
